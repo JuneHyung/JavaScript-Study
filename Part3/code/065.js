f0b8ce70830dd50c628805ad165a460f43af57a4
조준형 <cjh951114@naver.com>
2021-04-24 16:53:05 +0900

    Create 065.js : 문자열을 숫자형 정수로 변환하기(ParseInt)
    065 문자열을 숫자형 정수로 변환하기(ParseInt)

---
+++ Part3/code/065.js
@@ -0,0 +1,5 @@
+console.log(parseInt('15')); // 15
+console.log(parseInt('15',10)); // 15
+console.log(parseInt('15',2)); // 1
+console.log(parseInt(5.15)); // 5
+console.log(parseInt('5.15')); // 5
