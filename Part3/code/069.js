01ee99f8b377a70dd2ec80aae31822c65f9e51f1
조준형 <cjh951114@naver.com>
2021-04-24 16:54:59 +0900

    Create 069.js : 문자열 자르기 ② (substring)
    069 문자열 자르기 ② (substring)

---
+++ Part3/code/069.js
@@ -0,0 +1,8 @@
+const sentence = 'This will be the end of Wakanda';
+console.log('길이 : ' + sentence.length);
+console.log(sentence.substring(13)); // the end of Wakanda
+console.log(sentence.substring(13, 20)); // the end
+console.log(sentence.substring(0)); // This will be the end of Wakanda
+console.log(sentence.substring(0, -20)); //
+console.log(sentence.substring(50)); // 
+console.log(sentence.substring(20, 13)); // the end
