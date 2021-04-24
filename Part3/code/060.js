72d0cfb0265ae70f4c78f40ec68cabf5d57e2e8d
조준형 <cjh951114@naver.com>
2021-04-24 16:49:17 +0900

    060.js
    060 표준 내장 객체 이해하기

---
+++ Part3/code/060.js
@@ -0,0 +1,11 @@
+const str = new String('자바스크립트');
+const num = new Number(200);
+const bool = new Boolean(true);
+const date = new Date();
+const map = new Map();
+const set = new Set();
+
+// 어떤 객체는 선언없이 객체의 메소드와 속성을 바로 가져다 사용할 수 있습니다.
+console.log(Math.PI);
+console.log(Date.parse('2019-01-01'));
+console.log(JSON.pares('{}'));
