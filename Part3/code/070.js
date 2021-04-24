25de986d2f449c4fc243131cf78d2c40ff33ad61
조준형 <cjh951114@naver.com>
2021-04-24 16:55:14 +0900

    Create 070.js : 문자열 자르기 ③ (substr)
    070 문자열 자르기 ③ (substr)

---
+++ Part3/code/070.js
@@ -0,0 +1,9 @@
+const sentence = 'Wakanda Forever!!!';
+console.log(sentence.substr(8)); // Forever!!!
+console.log(sentence.substr(8, 7)); // Forever
+console.log(sentence.substr(0)); // Wakanda Forever!!!
+console.log(sentence.substr(-10)); // Forever!!!
+console.log(sentence.substr(0,-3)); // 
+console.log(sentence.substr(30)); // 
+console.log(sentence.substr(0,30)); // Wakanda Forever!!!
+
