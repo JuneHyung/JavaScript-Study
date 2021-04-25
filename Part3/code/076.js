1dd5f0faf5b5865ccc2196e37e7e1ebc09d303a2
조준형 <cjh951114@naver.com>
2021-04-25 21:26:58 +0900

    Create 076.js : lastIndexOf
    특정 문자열 위치 확인하기② (lastIndexOf)

---
+++ Part3/code/076.js
@@ -0,0 +1,5 @@
+const str = 'Carpe diem, seize the day';
+console.log(`"e"는 ${str.indexOf('e')} 번째 인덱스에 있습니다.`);
+console.log(`"C"는 ${str.indexOf('C')} 번째 인덱스에 있습니다.`);
+console.log(`"c"는 ${str.indexOf('c')} 번째 인덱스에 있습니다.`);
+console.log(`", se"는 ${str.indexOf(', se')} 번째 인덱스에 있습니다.`);
