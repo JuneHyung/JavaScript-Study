737d137a709404c68f6e89d228fa44828d0ec5c0
조준형 <cjh951114@naver.com>
2021-04-26 22:03:48 +0900

    Create 090.js : indexOf
    Create 090.js : indexOf

---
+++ Part3/code/090.js
@@ -0,0 +1,4 @@
+const arr = ['spring', 'summer', 'fail', 'winter', 'is', 'down'];
+
+console.log(`"Winter" is in this index ${arr.indexOf('winter')}`);
+console.log(`"Winter" is not here, look this value ${arr.indexOf('winter', 4)}`);
