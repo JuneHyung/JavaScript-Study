61c908e822f8ea0f069023d1374c885cc12a015c
조준형 <cjh951114@naver.com>
2021-05-10 22:44:38 +0900

    Create 111.js : ceil
    111 특정 자리수에서 올림하기 (ceil)

---
+++ Part3/code/111.js
@@ -0,0 +1,9 @@
+const positiveNum = 93.54;
+const negativeNum = -39.27;
+
+console.log(Math.ceil(positiveNum)); // 94
+console.log(Math.ceil(negativeNum)); // -39
+console.log(Math.ceil(positiveNum*10)/10); // 93.6
+console.log(Math.ceil(positiveNum/10)*10); // 100
+console.log(Math.ceil(negativeNum*10)/10); // -39.2
+console.log(Math.ceil(negativeNum/10)*10); // -30
