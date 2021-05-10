8c91902d9f8c836e8c50a96482c767818243cc98
조준형 <cjh951114@naver.com>
2021-05-10 22:44:50 +0900

    Create 112.js : floor
    112 특정 자리수에서 내림하기 (floor)

---
+++ Part3/code/112.js
@@ -0,0 +1,9 @@
+const positiveNum = 93.54;
+const negativeNum = -39.27;
+
+console.log(Math.floor(positiveNum)); // 93
+console.log(Math.floor(negativeNum)); // -40
+console.log(Math.floor(positiveNum*10)/10); // 93.5
+console.log(Math.floor(positiveNum/10)*10); // 90
+console.log(Math.floor(negativeNum*10)/10); // -39.3
+console.log(Math.floor(negativeNum/10)*10); // -40
