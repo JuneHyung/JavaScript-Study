26b3b9c3218bc896d33f3b278a157fd40d8f9aad
JuneHyung <cjh951114@gmail.com>
2021-08-01 15:47:26 +0900

    Create 139.js : Promise
    Create 139.js : Promise

---
+++ Part3/code/139.js
@@ -0,0 +1,32 @@
+function doJob(name, person) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (person.stamina > 50) {
+                resolve({
+                    result: `${name} success`,
+                    less: 30
+                });
+            } else {
+                reject(new Error(`${name} failed`));
+            }
+        }, 1000);
+    });
+};
+
+const harin = { stamina: 100 };
+doJob('work', harin)
+    .then(v => {
+        console.log(v.result);
+        harin.stamina -= v.loss;
+        return doJob('study', harin);
+    })
+    .then(v => {
+        console.log(v.result);
+        harin.stamina -= v.loss;
+        return doJob('work', harin);
+    })
+    .then(v => {
+        console.log(v.result);
+        harin.stamina -= v.loss;
+        return doJob('study', harin);
+    }).catch(e => console.log(e));
